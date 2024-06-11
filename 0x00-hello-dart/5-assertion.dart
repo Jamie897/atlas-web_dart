14c2fa8dcda577f26f7a338d5330db2b903996dd
Jamie897 <jamiedanielle456@gmail.com>
2024-06-10 19:38:39 -0500

    .

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,16 @@
 void main(List<String> args) {
- int score = int.parse[0]);
- assert(score >= 80, 'The score must be bigger or equal to 80');
- print('You Passed'); 
+  
+  if (args.isEmpty) {
+    print('Please provide a number as an argument');
+  }
+  
+  int nb = int.parse(args[0]);
+
+  // check if number is bigger or equal to 80
+  // otherwise print error message to assertion fail
+  assert(nb >= 80,
+      'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+
+  // if assertion pass, print "You passed the test with $number"
+  print('You Passed');
 }
