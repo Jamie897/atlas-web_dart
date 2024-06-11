a53320b36103ce8e27c569815d7d80a43cd69584
Jamie897 <jamiedanielle456@gmail.com>
2024-06-11 09:09:15 -0500

    .

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -1,20 +1,22 @@
+// functions.dart
 int add(int a, int b) {
-  return a+b;
-}   
+  return a + b;
+}
 
 int sub(int a, int b) {
-  return a-b;
-  }  
+  return a - b;
+}
 
-  String showFunc(int a, int b) {
-    int sum = add(a, b);
-    int difference = sub(a, b);
-    return 'Sum: $sum, Difference: $difference';
-  }
+String showFunc(int a, int b) {
+  int sum = add(a, b);
+  int difference = sub(a, b);
+  return 'Add $a + $b = $sum\nSub $a - $b = $difference';
+}
 
-  void main() {
-    int a = 10;
-    int b = 5;
-    String result = showFunc(a, b);
-    print(result);
-  }
+void main() {
+  int a = 8;
+  int b = 5;
+  String result = showFunc(a, b);
+  print(result);  // Output: Add 8 + 5 = 13
+                  //         Sub 8 - 5 = 3
+}
