ce6ecf08f46fac00b440ddaedd2695188bb789b2
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 10:42:42 -0500

    .

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,27 +1,15 @@
 class Password {
-  
   String? _password;
 
-  
-  String? get password => _password;
-
-  
-  set password(String? value) {
-    _password = value;
-  }
-
-  
   bool isValid() {
     if (_password == null) {
       return false;
     }
     
-    
     if (_password!.length < 8 || _password!.length > 16) {
       return false;
     }
     
-    // Check for uppercase, lowercase, and numbers
     bool hasUppercase = _password!.contains(RegExp(r'[A-Z]'));
     bool hasLowercase = _password!.contains(RegExp(r'[a-z]'));
     bool hasDigits = _password!.contains(RegExp(r'\d'));
@@ -29,9 +17,16 @@ class Password {
     return hasUppercase && hasLowercase && hasDigits;
   }
 
-  // Override the toString method
   @override
   String toString() {
     return 'Your Password is: $_password';
+  }
+
+  // Getter for the password property
+  String? get password => _password;
+
+  // Setter for the password property
+  set password(String? value) {
+    _password = value;
   }
 }
