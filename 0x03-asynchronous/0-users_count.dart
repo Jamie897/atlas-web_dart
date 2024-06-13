26b9f5ca2c8b01d04194467f519a94735d0877f7
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 11:06:26 -0500

    .

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,8 +1,6 @@
+import '0-util.dart';
+
 Future<void> usersCount() async {
-  try {
-    int count = await fetchUsersCount();
-    print(count);
-  } catch (error) {
-    print('Error fetching user count: $error');
-  }
+  int count = await fetchUsersCount();
+  print(count);
 }
