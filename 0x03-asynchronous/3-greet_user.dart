0267b2b13dd8556fd20e7df494dcbb6eaede6ead
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 11:27:45 -0500

    .

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:async';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> json = jsonDecode(userData);
+    String username = json['name'];
+    return 'Hello $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool isUserValid = await checkCredentials();
+    print('There is a user: $isUserValid');
+    if (isUserValid) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+void main() async {
+  print(await loginUser());
+}
