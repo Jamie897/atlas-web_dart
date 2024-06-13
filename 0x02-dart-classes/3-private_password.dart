eb9f28f835f20a8bfea1b28ebb786fb40bd5df4c
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 10:40:22 -0500

    Wrong Output

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,35 +1,37 @@
 class Password {
   
-  String? password;
+  String? _password;
 
+  
   String? get password => _password;
 
+  
   set password(String? value) {
     _password = value;
   }
 
-  // Method to check if the password is valid
+  
   bool isValid() {
-    if (password == null) {
+    if (_password == null) {
       return false;
     }
     
     
-    if (password!.length < 8 || password!.length > 16) {
+    if (_password!.length < 8 || _password!.length > 16) {
       return false;
     }
     
     // Check for uppercase, lowercase, and numbers
-    bool hasUppercase = password!.contains(RegExp(r'[A-Z]'));
-    bool hasLowercase = password!.contains(RegExp(r'[a-z]'));
-    bool hasDigits = password!.contains(RegExp(r'\d'));
+    bool hasUppercase = _password!.contains(RegExp(r'[A-Z]'));
+    bool hasLowercase = _password!.contains(RegExp(r'[a-z]'));
+    bool hasDigits = _password!.contains(RegExp(r'\d'));
 
     return hasUppercase && hasLowercase && hasDigits;
   }
 
-  
+  // Override the toString method
   @override
   String toString() {
-    return 'Your Password is: $password';
+    return 'Your Password is: $_password';
   }
 }
