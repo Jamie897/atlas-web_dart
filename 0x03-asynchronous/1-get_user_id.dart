ee4786fe10aa04047130c74b37532cb165c67ed8
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 11:19:12 -0500

    Forgot imports

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
+import 'dart:async';
 import '0-util.dart';
 
 Future<String> getUserId() async {
   String userData = await fetchUserData();
-  // Assuming fetchUserData() returns data in JSON format like '{"id": "12345", "name": "John"}'
-  // Parsing JSON to extract user ID
   Map<String, dynamic> json = jsonDecode(userData);
   String userId = json['id'];
   return userId;
+}
+
+void main() async {
+  String userId = await getUserId();
+  print(userId);
 }
