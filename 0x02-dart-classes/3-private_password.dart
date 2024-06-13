a2d10b2d3fb5687f97a091faf6ea92ba25c8dedd
Jamie897 <jamiedanielle456@gmail.com>
2024-06-13 10:47:22 -0500

    .

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,32 +1,37 @@
 class Password {
-  String? _password;
+  // Property to store password as PRIVATE
+  late String _password;
 
-  bool isValid() {
-    if (_password == null) {
-      return false;
-    }
-    
-    if (_password!.length < 8 || _password!.length > 16) {
-      return false;
-    }
-    
-    bool hasUppercase = _password!.contains(RegExp(r'[A-Z]'));
-    bool hasLowercase = _password!.contains(RegExp(r'[a-z]'));
-    bool hasDigits = _password!.contains(RegExp(r'\d'));
-
-    return hasUppercase && hasLowercase && hasDigits;
+  // Constructor to initialize password
+  Password({required String password}) {
+    this._password = password;
   }
 
+  // method: validate password based on the given conditions
+  bool isValid() {
+    // check if password is between 8 and 16 characters
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    // password should contains Uppercase letters
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+    // password should contains lowercase letters
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+    // password should contain numbers/digits
+    if (!_password.contains(RegExp(r'\d'))) {
+      return false;
+    }
+    // if all the conditions are satisfied, return true
+    return true;
+  }
+
+  // method to override toString method
   @override
   String toString() {
     return 'Your Password is: $_password';
-  }
-
-  // Getter for the password property
-  String? get password => _password;
-
-  // Setter for the password property
-  set password(String? value) {
-    _password = value;
   }
 }
